9d49a62bcfd6efe9a512eafd1586bc34e8ccf05e
Moha-A1 <11469@holbertonstudents.com>
2025-09-11 11:30:00 +0200

    test add

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module pour additionner deux entiers
+Contient une fonction pour additionner deux nombres
+et gérer les erreurs de type
+"""
+
+
+
+
+def add_integer(a, b=98):
+    """
+    Additionne deux entiers
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    
+    return int(a) + int(b)
