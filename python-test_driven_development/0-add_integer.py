ecb563770d29ed85536ab71c00c0efaf973c5e62
Moha-A1 <11469@holbertonstudents.com>
2025-09-11 14:00:16 +0200

    suppression de commentaire dans le module 0-add

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+"""
+Module pour additionner deux entiers
+Contient une fonction pour additionner deux nombres
+et gérer les erreurs de type
+"""
 
 
 def add_integer(a, b=98):
-
+  
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
