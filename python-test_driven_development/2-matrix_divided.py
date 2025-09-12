8f53594822e9db65525e07cd13d22576cb8b6d29
Moha-A1 <11469@holbertonstudents.com>
2025-09-12 10:43:01 +0200

    gestion de matrice vide

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -10,28 +10,39 @@ def matrix_divided(matrix, div):
     """
     Divise tous les éléments d'une matrice
     """
-    if not isinstance(matrix, list) or not all(
-            isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of "
-                        "integers/floats")
+    errors(matrix, div)
 
-    if not matrix or not all(row for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of "
-                        "integers/floats")
+    new_matrix = [row.copy() for row in matrix]
+    for row in range(len(matrix)):
+        for col, num in enumerate(matrix[row]):
+            new_matrix[row][col] = round(num / div, 2)
+    return new_matrix
 
-    if not all(isinstance(element, (int, float)) for row in matrix
-               for element in row):
-        raise TypeError("matrix must be a matrix (list of lists) of "
-                        "integers/floats")
 
+def errors(matrix, div):
+    """
+    Error management function
+    """
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-
-    if div == 0:
+    elif div == 0:
         raise ZeroDivisionError("division by zero")
 
-    row_length = len(matrix[0])
-    if not all(len(row) == row_length for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
+    if not isinstance(matrix, list) or not matrix:
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
 
-    return [[round(element / div, 2) for element in row] for row in matrix]
+    last_row = matrix[0]
+    for row in matrix:
+        if not isinstance(row, list):
+            raise TypeError(
+                    "matrix must be a matrix"
+                    " (list of lists) of integers/floats"
+                    )
+        if len(row) != len(last_row):
+            raise TypeError("Each row of the matrix must have the same size")
+        for num in row:
+            if not isinstance(num, (int, float)):
+                raise TypeError(
+                        "matrix must be a matrix"
+                        " (list of lists) of integers/floats")
