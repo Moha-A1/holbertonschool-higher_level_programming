a8987bb21437475297bd2a352747e6bb1a9df864
Moha-A1 <11469@holbertonstudents.com>
2025-09-15 15:49:54 +0200

    square

---
+++ python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module qui définit une classe Square vide.
+Ce module contient une classe Square de base pour apprendre la POO.
+"""
+
+
+class Square:
+    """
+    Une classe vide qui définit un carré.
+
+    Cette classe sert de fondation pour créer des objets carrés
+    et sera étendue avec des attributs et méthodes dans les exercices futurs.
+    """
+    pass
